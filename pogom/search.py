accfdbc033c7171f08aa9bc1b46b4b24721af9dc
Oliver Heering <heering@riege.com>
2017-03-10 11:27:17 +0100

    Verbose failure reasons

--- pogom/search.py
+++ pogom/search.py
@@ -820,7 +820,7 @@ def search_worker_thread(args, account_queue, account_failures,
                     log.warning(status['message'])
                     account_failures.append({'account': account,
                                              'last_fail_time': now(),
-                                             'reason': 'failures'})
+                                             'reason': 'failed more than {} times'.format(args.max_failures)})
                     # Exit this loop to get a new account and have the API
                     # recreated.
                     break
@@ -1134,7 +1134,7 @@ def search_worker_thread(args, account_queue, account_failures,
             traceback.print_exc(file=sys.stdout)
             account_failures.append({'account': account,
                                      'last_fail_time': now(),
-                                     'reason': 'exception'})
+                                     'reason': repr(e)})
             time.sleep(args.scan_delay)
 
 
